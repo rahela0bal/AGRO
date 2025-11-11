22dea5a96bc09551d86a186f262e5993f99b8489
Rahela <rahela.balta@emanuel.ro>
2025-11-11 11:03:04 +0200

    modificari facute pentru port

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"",
+  base: "",
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,4 +15,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true  // Important pentru Docker + hot reload
+    }
+  }
 })
